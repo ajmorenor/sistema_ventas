01052fae9692dab1e0050e2a45a42bc919d48965
Antonio Moreno <moreantonio@gmail.com>
2024-08-27 12:04:58 -0400

    first commit

---
+++ protected/views/admin/culqui.php
@@ -0,0 +1,38 @@
+
+<div id="error-message-wrapper"></div>
+CULQUI
+<form class="uk-form uk-form-horizontal forms" id="forms">
+<?php echo CHtml::hiddenField('action','AdminCulquiSettings')?>
+
+<div class="uk-form-row">
+  <label class="uk-form-label"><?php echo t("Enabled")." ".t("culqui")?>?</label>
+  <?php 
+  echo CHtml::checkBox('admin_cul_enabled',
+  getOptionA('admin_cul_enabled')==2?true:false
+  ,array(
+    'value'=>2,
+    'class'=>"icheck"
+  ))
+  ?> 
+</div>
+
+
+
+<div class="uk-form-row">
+  <label class="uk-form-label"><?php echo t("Merchant ID")?></label>
+  <?php 
+  echo CHtml::textField('admin_cul_merchant_id',
+  Yii::app()->functions->getOptionAdmin('admin_cul_merchant_id')
+  ,array(
+    'class'=>"uk-form-width-large"
+  ))
+  ?>
+</div>
+
+
+<div class="uk-form-row">
+<label class="uk-form-label"></label>
+<input type="submit" value="<?php echo Yii::t("default","Save")?>" class="uk-button uk-form-width-medium uk-button-success">
+</div>
+
+</form>
